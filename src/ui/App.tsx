eaf69269843cc8494275392b5adeb9e380e63cfb
ductham08 <ductham784@gmail.com>
2025-04-17 23:41:33 +0700

    check update version

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -15,7 +15,6 @@ function App() {
         </a>
       </div>
       <h1>hihe</h1>
-      <h1>hihe</h1>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
