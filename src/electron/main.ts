02d3373402640b341280de9b7a13ab03a89be7c1
ductham08 <ductham784@gmail.com>
2025-04-17 23:39:12 +0700

    update version auto update

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,14 +1,50 @@
-import {app, BrowserWindow} from "electron"
+import { app, BrowserWindow, dialog } from "electron"
 import path from "path"
 import { isDev } from "./util.js"
+import pkg from 'electron-updater'
+const { autoUpdater } = pkg
 
-type test = string
+let mainWindow
 
-app.on("ready", () => {
-    const mainWindow = new BrowserWindow({})
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    })
+
     if (isDev()) {
         mainWindow.loadURL("http://localhost:5123")
     } else {
         mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'))
     }
+}
+
+app.on("ready", () => {
+    createWindow()
+
+    if (!isDev()) {
+        autoUpdater.checkForUpdates()
+    }
+})
+
+autoUpdater.on('update-available', () => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Update Available',
+        message: 'Có phiên bản mới. Ứng dụng sẽ được cập nhật sau khi tải xong.',
+    })
+})
+
+autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Update Ready',
+        message: 'Update đã tải xong. Ứng dụng sẽ khởi động lại để cập nhật.',
+    }).then(() => {
+        autoUpdater.quitAndInstall()
+    })
 })
